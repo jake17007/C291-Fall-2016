43ca293eddb4033c7e4f21ee08f59868486e2ab3
Jacob Beauchamp <jacobeau@silo.soic.indiana.edu>
2016-09-01 16:49:34 -0400

    Added comments to prime.c

--- ex2/prime.c
+++ ex2/prime.c
@@ -7,11 +7,13 @@ int main(int theNumber) {
     int j = 2;
     int prime = 1;
     while (prime != 0 && j<i) {
-      
+      // Check if any number under the potential prime divides evenly
+      // If this is so, change "prime" to 0 (meaning false)
       if (i % j == 0)
 	prime = 0;
       j++;
     }
+    // If the potential prime is still a prime at this point, print it
     if (prime == 1)
       printf("%d\n",i);
   }
